7b6c20b2913e6bccb1b08448782d2aef39d0375e
Magvin <zans.litinskis@gmail.com>
2017-12-27 19:08:36 +0000

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01, message:"%{value} seems wrong"} 
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i,
+	message:"must be GIF, JPG or PNG file."} #i stands for case insensitive
 end
