00af6f60d03aedcc0184b0a763667dca7d9cf00f
Magvin <zans.litinskis@gmail.com>
2018-01-04 21:38:51 +0000

    Fixing test,layout fixing

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,12 @@ class Product < ApplicationRecord
 	before_destroy :ensure_not_referenced_by_any_line_item
 
 	validates :title, :description, :image_url, presence: true
-	validates :price, numericality: {greater_than_or_equal_to: 0.01, message:"%{value} seems wrong"} 
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
-	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i,
-	message:"must be GIF, JPG or PNG file."} #i stands for case insensitive
+	validates :image_url, allow_blank: true, format: { 
+		with: %r{\.(gif|jpg|png)\z}i,
+		message: 'must be GIF, JPG or PNG file.'
+	} #i stands for case insensitive
 	validates :title, :length => {:minimum => 2}
 	validates :description, :length => { :in => 3..150 }
 
